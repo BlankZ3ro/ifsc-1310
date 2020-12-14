aa947711dc53d4b38c1a676946cf7addb0dbbde8
Jbmcmath2 <jbmcmath@ualr.edu>
2020-12-14 01:49:10 +0000

    Dragon V2 (Remaster), Dragon Reflective Writing, and Cycle JS Lab

---
+++ cycle-lab/assets/js/local-calls.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    //your code goes inbetween the "{}" 
+    
+    $('.gallery').cycle({
+       fx: 'fade' //choose your transition type, ex: fade, scrollUp, shuffle, etc... 
+    });
+    
+});
---
+++ dragon-project-remastered/assets/js/local-calls.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    //your code goes inbetween the "{}" 
+$('.gallery').cycle({ 
+    fx:     'fade', 
+    speed:  'fast', 
+    timeout: 0, 
+    next:   '#next2', 
+    prev:   '#prev2' 
+});
+});
